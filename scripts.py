278b69256dda1a46eb5315817f5e46e376a5c489
raccess21 <rahulanand0021@outlook.com>
2025-01-13 01:08:10 +0530

    musicbee xspf export clean script added

--- scripts.py
+++ scripts.py
@@ -23,13 +23,10 @@ def name_clean():
         f.write(json.dumps(names, indent=2))
 
 
-def remove_tags(tree, tags, ns):
+def remove_tags(root, tags, ns):
     if not isinstance(tags, list):
         tags = [tags]
-
-    parser = etree.XMLParser(remove_blank_text=True)
-    root = tree.getroot()
-        
+    
     for tag in tags:
         # Find all instances of the tag using XPath with namespace
         elements = root.xpath(f".//default:{tag}", namespaces=ns)
@@ -39,7 +36,18 @@ def remove_tags(tree, tags, ns):
 
     
 
-def xml_clean_vlc(file_name = "all.xspf", playlist_title = "Playlist"):
+def xml_clean_vlc(root, ns):
+    # remove unnecessary tags from track data
+    bad_tags = ["image", "annotation", "extension"]    
+    remove_tags(root, bad_tags, ns)
+
+    # File location edit for local files relative paths
+    for location_tag in root.xpath("//default:location", namespaces=ns):
+        location_tag.text = location_tag.text.replace("file:///C:/rahul/Audio/", "../")
+           
+
+
+def xml_clean(file_name = "all.xspf", playlist_title = "Playlist"):
     # Define namespaces
     ns = {
         'default': "http://xspf.org/ns/0/",
@@ -50,40 +58,31 @@ def xml_clean_vlc(file_name = "all.xspf", playlist_title = "Playlist"):
     root = tree.getroot()
     
     title_tag = root.xpath("//default:title", namespaces=ns)[0]
-
-    # remove unnecessary tags from track data
-    bad_tags = ["image", "annotation", "extension"]    
-    remove_tags(tree, bad_tags, ns)
-
-    songs = []
-    # File location edit for local files relative paths
-    for location_tag in root.xpath("//default:location", namespaces=ns):
-        location_tag.text = location_tag.text.replace("file:///C:/rahul/Audio/", "../")
-        songs.append(location_tag.text)
+    
+    if root.xpath("//default:extension", namespaces=ns):
+        xml_clean_vlc(root, ns)
            
     title_tag.text = playlist_title
     tree.write(f"playlists/{file_name}", pretty_print=True, xml_declaration=True, encoding="UTF-8")
     
-    with open(f"playlists/{file_name.split('.')[0]}.m3u", 'w') as fo:
-        fo.write("\n".join(songs))
-        
-    print(f"{file_name} XSPF Cleaned and written")
 
     songs = []
     # File location edit for server files 
     for location_tag in root.xpath("//default:location", namespaces=ns):
-        location_tag.text = location_tag.text.replace("../", "https://raw.githubusercontent.com/raccess21/Audio/main/")
         songs.append(location_tag.text)
+        location_tag.text = location_tag.text.replace("../", "https://raw.githubusercontent.com/raccess21/Audio/main/")
         
     # Write the modified XML to a new file
     title_tag.text = playlist_title + " Web"
-    file_name = f"{file_name.split('.')[0]}_web.xspf"
-    tree.write(f"playlists web/{file_name}", pretty_print=True, xml_declaration=True, encoding="UTF-8")
+    tree.write(f"playlists web/{file_name.split('.')[0]} web.xspf", pretty_print=True, xml_declaration=True, encoding="UTF-8")
+    print(f"{file_name} Web and local XSPF Cleaned and written")
     
-    with open(f"playlists web/{file_name.split('.')[0]}.m3u", 'w') as fo:
+    with open(f"playlists/{file_name.split('.')[0]}.m3u", 'w') as fo:
         fo.write("\n".join(songs))
-    print(f"{file_name} XSPF Cleaned and written web")
+    
+    print(f"{file_name} Local m3u cleaned and written.")
 
+#def xsfp_process(file_name = "all.xspf", playlist_title = "Playlist"):
 
 def embed_lyrics(lyrics, file_name):
     audiofile = eyed3.load(file_name)
@@ -99,16 +98,10 @@ def create_xml():
     ...
     
 def main():
-    file_names = [
-        "The Wall - Pink Floyd (flac).xspf",
-        "all_songs.xspf",
-        "Dark Side of the Moon - Pink Floyd (MP3).xspf"
-    ]
-
-    for file_name in file_names:
+    for file_name in os.listdir("playlists temp"):
         playlist_title = file_name.split('.xspf')[0].strip()
         # name_clean()
-        xml_clean_vlc(file_name, playlist_title)
+        xml_clean(file_name, playlist_title)
         # create_xml()
         # ...
         # embed_lyrics("[00:43.22]lala", "Asi_Gabru_Punjabi_-_Amrinder_Gill.m4a")
