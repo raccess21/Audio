7763bd065648f8870d90fb78ba447a42c34ddb87
raccess21 <rahulanand0021@outlook.com>
2025-01-12 15:40:08 +0530

    ..

--- scripts.py
+++ scripts.py
@@ -23,31 +23,42 @@ def name_clean():
         f.write(json.dumps(names, indent=2))
 
 
-def xml_clean(file_name = "all.xspf", playlist_title = "Playlist"):
+def remove_tags(tree, tags, ns):
+    if not isinstance(tags, list):
+        tags = [tags]
+
+    parser = etree.XMLParser(remove_blank_text=True)
+    root = tree.getroot()
+        
+    for tag in tags:
+        # Find all instances of the tag using XPath with namespace
+        elements = root.xpath(f".//default:{tag}", namespaces=ns)
+        for element in elements:
+            # Remove each found element
+            element.getparent().remove(element)
+
+    
+
+def xml_clean_vlc(file_name = "all.xspf", playlist_title = "Playlist"):
+    # Define namespaces
+    ns = {
+        'default': "http://xspf.org/ns/0/",
+    }
+    
     parser = etree.XMLParser(remove_blank_text=True)
     tree = etree.parse(f"playlists temp/{file_name}", parser)
     root = tree.getroot()
     
-    # Define namespaces
-    ns = {
-        'default': "http://xspf.org/ns/0/",
-        'vlc': "http://www.videolan.org/vlc/playlist/ns/0/"
-    }
-    
     title_tag = root.xpath("//default:title", namespaces=ns)[0]
 
     # remove unnecessary tags from track data
-    for track in root.xpath("//default:track", namespaces=ns):
-        image_tag = track.find("default:image", namespaces=ns)
-        if image_tag is not None:
-            track.remove(image_tag)
-        
-        annotation_tag = track.find("default:annotation", namespaces=ns)
-        if annotation_tag is not None:
-            track.remove(annotation_tag)
-        
+    bad_tags = ["image", "annotation", "extension"]    
+    remove_tags(tree, bad_tags, ns)
 
-    # File location edit for local files 
+    # File location edit for local files hard path 
+    tree.write(f"playlists temp2/{file_name}", pretty_print=True, xml_declaration=True, encoding="UTF-8")
+
+    # File location edit for local files relative paths
     for location_tag in root.xpath("//default:location", namespaces=ns):
         location_tag.text = location_tag.text.replace("file:///C:/rahul/Audio/", "../")
 
@@ -58,7 +69,7 @@ def xml_clean(file_name = "all.xspf", playlist_title = "Playlist"):
 
     # File location edit for server files 
     for location_tag in root.xpath("//default:location", namespaces=ns):
-        location_tag.text = "https://raw.githubusercontent.com/raccess21/Audio/main/" + location_tag.text
+        location_tag.text = location_tag.text.replace("../", "https://raw.githubusercontent.com/raccess21/Audio/main/")
 
     # Write the modified XML to a new file
     title_tag.text = playlist_title + " Web"
@@ -80,8 +91,10 @@ def main():
     for file_name in file_names:
         playlist_title = file_name.split('.xspf')[0].strip()
         # name_clean()
-        xml_clean(file_name, playlist_title)
+        xml_clean_vlc(file_name, playlist_title)
         # create_xml()
+        # ...
+    # remove_tags(tree=1, tags=["duration", "extension"])
 
 if __name__ == "__main__":
     main()
