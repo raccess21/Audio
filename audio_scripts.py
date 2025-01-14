324897390773c2d5b8ee2527ecc79bcf13b2bdb0
raccess21 <rahulanand0021@outlook.com>
2025-01-14 21:57:22 +0530

    lyrics scripts

--- audio_scripts.py
+++ audio_scripts.py
@@ -55,7 +55,7 @@ def xml_clean_vlc(root, ns):
     # File location edit for local files relative paths
     for location_tag in root.xpath("//default:location", namespaces=ns):
         location_tag.text = location_tag.text.replace("file:///C:/rahul/Audio/", "../")
-           
+
 
 from lxml import etree
 
@@ -79,7 +79,9 @@ def xspf_to_m3u(root, ns, playlist_name="Playlist"):
     return m3u.replace("%20", " ")
 
 
-def xml_clean(file_name = "all.xspf", playlist_title = "Playlist"):
+# web playlist in xspf for vlc -> better naming in interface
+# local playlist in m3u for musicbee/musicolet, don't support xspf
+def playlists_from_xspf(file_name = "all.xspf", playlist_title = "Playlist"):
     # Define namespaces
     ns = {
         'default': "http://xspf.org/ns/0/",
@@ -94,41 +96,32 @@ def xml_clean(file_name = "all.xspf", playlist_title = "Playlist"):
     if root.xpath("//default:extension", namespaces=ns):
         xml_clean_vlc(root, ns)
 
-    
-    # local xspf not needed
-    # title_tag.text = playlist_title
-    # tree.write(f"playlists/{file_name}", pretty_print=True, xml_declaration=True, encoding="UTF-8")
+    # File location edit for server files 
+    # plylist export absolute path will always contain Audio -> reform into relative path
+    for location_tag in root.xpath("//default:location", namespaces=ns):
+        location_tag.text = "../" + location_tag.text.split("Audio/")[1]
     
     #writing m3u local files
     with open(f"playlists/{file_name.split('.')[0]}.m3u", 'w') as fo:
         fo.write(xspf_to_m3u(root, ns, playlist_title))
-
-    print(f"{file_name} Local XSPF and m3u Cleaned and written")
+        print(f"{file_name} Local m3u Cleaned and written")
 
     # File location edit for server files 
     for location_tag in root.xpath("//default:location", namespaces=ns):
-        location_tag.text = location_tag.text.replace("../", "https://raw.githubusercontent.com/raccess21/Audio/main/")
+        location_tag.text = "https://raw.githubusercontent.com/raccess21/Audio/main/" + location_tag.text.split("../")[1]
         
     # Write web files
     title_tag.text = playlist_title + " Web"
     tree.write(f"playlists web/{file_name.split('.')[0]} web.xspf", pretty_print=True, xml_declaration=True, encoding="UTF-8")
-    
-    # web m3u not needed 
-    # with open(f"playlists web/{file_name.split('.')[0]} web.m3u", 'w') as fo:
-    #     fo.write(xspf_to_m3u(root, ns, playlist_title + " Web"))
     print(f"{file_name} Web XSPF Cleaned and written")
 
 
 def main():
+    playlist_extensions = ["xspf"]
     for file_name in os.listdir("playlists temp"):
-        playlist_title = file_name.split('.xspf')[0].strip()
-        # name_clean()
-        xml_clean(file_name, playlist_title)
-        # create_xml()
-        # ...
-        # embed_lyrics("[00:43.22]lala", "Asi_Gabru_Punjabi_-_Amrinder_Gill.m4a")
-        # rename_files_recursively(base_dir="")
-    # remove_tags(tree=1, tags=["duration", "extension"])
+        if file_name.split(".")[1] in playlist_extensions:
+            playlist_title = file_name.split('.xspf')[0].strip()
+            playlists_from_xspf(file_name, playlist_title)
 
 if __name__ == "__main__":
     main()
---
+++ lyrics_scripts.py
@@ -0,0 +1,101 @@
+import syncedlyrics
+import os
+import re
+from mutagen import File
+from mutagen.id3 import ID3, USLT, SYLT, Encoding
+from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
+import chardet
+
+# extensions supported
+
+music_extensions = ['.flac', '.mp3', ".m4a"]
+
+def search_lyrics(song_name, duration=0):
+    lyrics = syncedlyrics.search(song_name)
+    # pyperclip.copy(lyrics)
+    return lyrics
+
+def get_lyrics(file):
+    # tag list for file types
+    tags = {
+        "flac": {"function": FLAC, "title": "title", "artist": "artist"},
+        "mp3":  {"function": MP3, "title": "TIT2", "artist": "TPE1"},
+        "m4a":  {"function": File, "title": "©nam", "artist": "©ART"}
+    }
+    # pending audio tags, vorbis, wav, opus, ogg
+
+    ext = file.split('.')[-1].lower()
+    tags = tags[ext]
+    audio = tags["function"](file)
+    
+    # catch tag/corruption error
+    try:
+        if audio.tags is not None:
+            # catch not found/timeout error
+            try:
+                lyrics = search_lyrics(str(audio[tags["title"]]) + " " + str(audio[tags["artist"]]))
+                return lyrics
+            except Exception as e:
+                print(f"{e} : {file}")
+                return False
+    except Exception as e:
+        print(f"{e} : {file}")
+        return False  
+    
+# detect lyrics encoding and write as lrc file
+def write_lrc(filename, lyrics = ""):
+    use_encoding = chardet.detect(lyrics.encode())["encoding"]
+    with open(filename, "w", encoding=use_encoding) as f:
+        f.write(str(lyrics))
+
+# regex pattern substitution for extension 
+def remove_extension(filename, pattern=None):
+    pattern = pattern or "|".join(re.escape(ext) for ext in music_extensions)
+    return re.sub(pattern, "", filename)
+
+# walk all files in provided base directory
+def all_files_in(base_dir="./"):
+    # pattern for extension removal
+    pattern = "|".join(re.escape(ext) for ext in music_extensions)
+
+    file_counter = 0                                                       #file counter
+    for root, _, files in os.walk(base_dir):
+        for file in files:
+            file_path = os.path.join(root, file).replace("\\", "/")
+            if os.path.splitext(file)[1].lower() in music_extensions:
+                filename = remove_extension(file_path, pattern) + ".lrc"
+                
+                # if lrc file does not exist search and write lyrics
+                if not os.path.exists(filename):
+                    lyrics = get_lyrics(file_path)
+                    if lyrics:
+                        write_lrc(filename, lyrics)
+                        file_counter += 1
+                        print(f"{file_counter}. {filename} written")
+                    else:
+                        write_lrc(filename, "Not Found")
+
+def clean_spam_tags():
+    # web download tag remove
+    # lyricist tag update
+    
+    ...
+# print all tags of audio file for analysis
+def all_tags(filename):
+    audio = File(filename)
+    for tag, value in audio.tags.items():
+        if len(str(value)) < 10000:
+            print(tag, value, sep=": ")
+
+if __name__ == "__main__":
+    os.system('cls')
+    
+    test_files = [
+        "Brain Damage.mp3",
+        "Vera.flac"
+    ]
+
+    # get_lyrics("You_Belong_With_Me_-_Taylor_Swift.m4a")
+    # all_files_in()
+    all_tags("lossy/Co2.mp3")
