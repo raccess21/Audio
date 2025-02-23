dbc421c0cfc9ad41e5a650916420c9c1f9f96963
raccess21 <rahulanand0021@outlook.com>
2025-02-24 01:30:55 +0530

    added lang

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from mutagen.id3 import TCON
 
 def lang_detect(file):
         langs = {
-            "hi": "Hindi", "en": "English", "ost": "Original Soundtrack"
+            "hi": "Hindi", "en": "English", "ost": "Soundtrack"
         }
         
         name, ext = file_name_ext(file)
@@ -21,34 +21,35 @@ def lang_detect(file):
                         lang = "hi"
                 except:
                     lang = "ost"
-        
+        print(lang)
         return langs[lang]
 
 music = music_extensions()
 
 for file in all_files_in("lossy/"):
     name, ext = file_name_ext(file)
+
     if ext in music:
         print(name, end=": ")
         tags = audio_tags()[ext]
         audio = tags["function"](file)
         lang = lang_detect(file.split("." + ext)[0] + ".lrc")
-        print(lang)
         
-        
-        if ext == "mp3":
-            current = audio[tags["genre"]].text
-            if lang not in current:
-                current.append(lang)
-            audio[tags["genre"]] = TCON(encoding=3, text=current)
+        try:
+            if ext == "mp3":
+                current = audio[tags["genre"]].text
+                if lang not in current:
+                    current.append(lang)
+                audio[tags["genre"]] = TCON(encoding=3, text=current)
 
-        elif ext == "m4a" or ext == "flac":
-            current = audio[tags["genre"]]
-            if lang not in current:
-                current.append(lang)
-            audio[tags["genre"]] = current
+            elif ext == "m4a" or ext == "flac":
+                current = audio[tags["genre"]]
+                if lang not in current:
+                    current.append(lang)
+                audio[tags["genre"]] = current
+            
+            audio.save()
         
-        audio.save()
-        # print(all_tags(file))
-        # break
+        except Exception as e:
+            print(file, e)
 
