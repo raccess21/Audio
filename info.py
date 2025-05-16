fc556ad0463c73343f606892c7095a5bf6a74142
raccess21 <rahulanand0021@outlook.com>
2025-05-16 14:02:53 +0530

    code for update songs.json object structure genre, artists -> list of str

--- info.py
+++ info.py
@@ -6,6 +6,7 @@ from mutagen.id3 import ID3, TCON, TALB
 import os
 from ytmusicapi import YTMusic
 import json
+import ast
 
 def music_extensions():
     return ["flac", "mp3", "m4a"]
@@ -71,26 +72,57 @@ def sorted_m3u(m3u):
     new_m3u.sort()
     return "\n".join(m3u[:2] + [line for _, line1, line2 in new_m3u for line in [line1, line2]]) 
 
-def all_songs_dict():
+def clean_tag(ext, file_path):
+    tags = audio_tags()[ext]
+    audio = tags["function"](file_path)
+    
+    song = {
+        "title" : str(audio[tags["title"]]),
+        "artists" : str(audio[tags["artists"]]),
+        "genre" : str(audio[tags["genre"]]),
+        "album" : str(audio[tags["album"]]),
+        "path": file_path.split("Audio/")[-1],
+        "duration": round(audio.info.length),
+    }
+    
+    # Helper to safely parse and clean list-like fields
+    def clean_mp3(song):
+        song["artists"] = [a.strip() for a in song["artists"].replace(',', ';').split(";")]
+        song["genre"] = [a.strip() for a in song["genre"].replace("\u0000", ";").split(";")]
+        return song
+    
+    def clean_m4a(song):
+        song["title"] = ast.literal_eval(song["title"])[0]
+        song["artists"] = ast.literal_eval(song["artists"])
+        song["genre"] = ast.literal_eval(song["genre"].replace("; ", "', '"))
+        song["album"] = song["album"].split("'")[1]
+        return song
+
+    def clean_flac(song):
+        ...
+
+    cleaners = {
+        "flac": clean_flac,
+        "mp3": clean_mp3,
+        "m4a": clean_m4a    
+    }
+    song = cleaners[ext](song)
+    title = song.pop("title")
+    return title, song
+
+def all_songs_dict(base_dir=["lossy/"]):
     try:
         with open("web_assets/songs.json", "r") as f:
             songs = json.loads(f.read())
     except FileNotFoundError:
         songs = {}
     
-    for i, file_path in enumerate(all_files_in(base_dir=["lossy/"])):
+    for i, file_path in enumerate(all_files_in(base_dir=base_dir)):
         name, ext = file_name_ext(file_path)
         if ext in music_extensions() and file_path not in songs:
             print(i, name, ' ', sep='"')
-            tags = audio_tags()[ext]
-            audio = tags["function"](file_path)
-            songs[str(audio[tags["title"]])] = {
-                "path": file_path.split("Audio/")[-1],
-                "artists": str(audio[tags["artists"]]).replace("\u0000", ";"),
-                "duration": round(audio.info.length),
-                "album": str(audio[tags["album"]]),
-                "genre": str(audio[tags["genre"]]).replace("\u0000", ";"),
-            }
+            title, song = clean_tag(ext, file_path)
+            songs[title] = song
 
     with open("web_assets/songs.json", "w") as f:
         f.write(json.dumps(songs, indent=4))
@@ -124,10 +156,10 @@ def get_playlist(id="PLlXEnX_5coLUM5Sn_YV1ldufT55OxP9VS"):
 
 
 if __name__ == "__main__":
-    with open("web_assets/yt_playlists.json", "r") as f:
-        playlists = json.loads(f.read())
+    # with open("web_assets/yt_playlists.json", "r") as f:
+    #     playlists = json.loads(f.read())
     
-    for playlist in playlists:
-        get_playlist(playlists[playlist])
-        print("\n\n")
-    # all_songs_dict()
+    # for playlist in playlists:
+    #     get_playlist(playlists[playlist])
+    #     print("\n\n")
+    all_songs_dict()
